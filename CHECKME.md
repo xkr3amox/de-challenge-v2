3ca57109c117605605be5b9a06f4d826c0784f04
Eliseo Suazo <emsuazo@falabella.cl>
2022-03-15 21:42:10 -0300

    add job

---
+++ CHECKME.md
@@ -0,0 +1,91 @@
+# DE Challenge
+
+This is the New Challenge for Data Engineers.
+
+The EPL (English Premier League) has information of seasons between 09-10 and 18-19. They hire you as a DE to process the data per season to get some information relevant for the EPL and also send it to the teams and media involved.
+
+For this Challenge: 
+- We want you to do a Job that gives us the Data for the Analytics team, but with a few concerns:
+    - The Job must be an ETL code in Java or Scala or Python.
+    - We want a Deployment for this code.
+- We want you to propose an arquitecture for the case below.
+- We want a deployment for the ETL, please remember that best deployments are the easier to execute.
+
+
+# Architecture Case
+We want an architecture proposition for this case:
+
+The EPL give different awards each season to the players and teams who played, the awards are:
+- Best Scoring Team
+- Best Coach
+- Best Scoring Player
+- Less Beaten Goalkepper
+- Best Assist Player
+- Player who traveled the most kilometers per game played
+- Golden Boy (Best Sub20 Player)
+- Others to be defined each season
+
+During each match, the EPL records information of each of the teams involved (Roster of players with name and last name, number, name of the coach), the goals in favor and against before the match. Also, during the course of the match it records in real time the shots, shots on goal, goals, assists, fouls, cards and goalkeeper saves.
+
+Each player has a device that allows to record the distance covered during the minutes he was on the field, lost water and minutes played. This information is sent in real time to a central server (provided by the owners of the device) and sent to each of the analysis centers of each team for future analysis.
+
+The information of the match and players stats are crossed and sent to different sports sites in real time, in order to have the most recent information of each ongoing match, as well as to the bet houses to change every 5 minutes the % of bet on each of the alternatives they offer. 
+
+Propose an architecture for the given situation, preferably with open source tools. And save in the architecture case folder (image in png or jpg)
+
+# ETL Job
+
+The job must receive the datasets & brings a few things:
+
+- The position table for all the seasons.
+- **The other requeriments will be in the email**
+
+The data is in the folder data/ in the root. The report needs to be exported to a file, you can chose the extension of the file, but remember this is an ETL Job.
+
+Please remember to save your code in src folder.
+
+
+## Acronyms Meaning
+
+Name | Meaning
+--- | --- 
+DIV | division (E0, premier league)  
+Date | date   
+HOMETEAM | Equipo Local   
+AWAYTEAM | Equipo Visita   
+FTHG | Full Time Home Goals  
+FTAG | Full Time Away Goals  
+FTR | Full Time Result (H, A, D)[Home, Away, Draw] 
+HTHG | Half Time Home Goals  
+HTAG | Half Time Away Goals  
+HTR | Half Time Result  
+Referee | Arbitro   
+HS | Home Shots   
+AS | Away Shots   
+HST | Home Shots Target  
+AST | Away Shots Target  
+HF | Home Fault   
+AF | Away Faut   
+HC | Home Card   
+AC | Away Card   
+HY | Home Yellow   
+AY | Away Yellow   
+HR | Home Red   
+AR | Away Red
+
+# Deployment
+We want you to tell us how to deploy your job and run it in any environment, so please provide very clear and detailed instructions. If you use any cloud resources, remember to give public access and ask for the reviewers mails to deploy the solution. In any case we preffer open source solutions.
+
+Plase save the deployment in the deploy folder.
+
+# Concerns
+- You can create a new README.md (choose a new name) for anything you want to tell us. Please don't name it README.md
+- We want to see if you know how to code in a professional way, so use the best practices of Software Engineering!
+- This is an ETL Job, so show us all you know about good practices to do ETL's.
+- Save all the changes in your personal GitHub account using a Fork from this repository and send us the link to clone and see your repository.
+
+## Disclaimer Note
+``` This challenge is your cover letter, the elections you choose to do & not to do matters!!, and will be asked in the next interview. If you don't finish the challenge, we will have to end your application```
+
+## Datasets
+Dataset for the challenge is inside folder "data"
---
+++ src/etl2.py
@@ -0,0 +1,144 @@
+import os
+import pandas as pd
+
+#GLOBAL
+dirpath = '/usr/src/app/data'
+#Helper lists
+cols = ["Div","Date","FTR","HomeTeam","FTHG","HS","HST","AwayTeam","FTAG","AS","AST","HTR","HTHG","HTAG"]
+board_cols = ['Team','Pts','W','L','D','GF','GA','DIFF','Season']
+goal_stat_cols = ['Season','Team','GF']
+score_reverted_cols = ['Team','n_of_matches','Season']
+shot_stats_cols = ['Team','shot_percentage','Season']
+#- Equipo más goleador por temporada.
+best_scoring_team_by_season = pd.DataFrame()
+#- Equipo con la mejor relación de disparos al arco terminando en gol por temporada.
+best_shot_stats_team_df = pd.DataFrame()
+#- Equipo que dio vuelta mas resultados por temporada
+best_lose_to_win_team_df = pd.DataFrame()
+
+def get_season_from_file(filename):
+    return filename.rpartition('-')[2].rpartition('_')[0]
+
+def get_teams(df):
+    return pd.concat([df['HomeTeam'],df['AwayTeam']]).unique()
+
+def get_pts_by_team(df,item):
+    hw = df.apply(lambda x: True if (x['HomeTeam'] == item and x['FTR'] == 'H') else False , axis=1)
+    aw = df.apply(lambda x: True if (x['AwayTeam'] == item and x['FTR'] == 'A') else False , axis=1)
+    d = df.apply(lambda x: True if (x['HomeTeam'] == item or x['AwayTeam'] == item and x['FTR'] == 'D') else False , axis=1)
+    return (len(hw[hw == True].index))*3 + (len(aw[aw == True].index))*3 + len(d[d == True].index)
+
+def get_w_by_team(df,item):
+    hw = df.apply(lambda x: True if (x['HomeTeam'] == item and x['FTR'] == 'H') else False , axis=1)
+    aw = df.apply(lambda x: True if (x['AwayTeam'] == item and x['FTR'] == 'A') else False , axis=1)
+    return len(hw[hw == True].index) + len(aw[aw == True].index)
+
+def get_l_by_team(df,item):
+    hw = df.apply(lambda x: True if (x['HomeTeam'] == item and x['FTR'] == 'A') else False , axis=1)
+    aw = df.apply(lambda x: True if (x['AwayTeam'] == item and x['FTR'] == 'H') else False , axis=1)
+    return len(hw[hw == True].index) + len(aw[aw == True].index)
+
+def get_d_by_team(df,item):
+    d = df.apply(lambda x: True if (x['HomeTeam'] == item or x['AwayTeam'] == item and x['FTR'] == 'D') else False , axis=1)
+    return len(d[d == True].index)
+
+def get_best_shot_stats_by_team(df,item):
+    shots = df.loc[df['HomeTeam'] == item, 'HST'].sum() + df.loc[df['AwayTeam'] == item, 'AST'].sum()
+    goals = df.loc[df['HomeTeam'] == item, 'FTHG'].sum() + df.loc[df['AwayTeam'] == item, 'FTAG'].sum()
+    return shots/goals
+
+def get_best_lose_to_win_team(df,item):
+    htimes = df.apply(lambda x: True if (x['HomeTeam'] == item and x['HTAG'] > x['HTHG'] and x['FTHG'] > x['FTAG']) else False , axis=1)
+    atimes = df.apply(lambda x: True if (x['AwayTeam'] == item and x['HTHG'] > x['HTAG'] and x['FTAG'] > x['FTHG']) else False , axis=1)
+    return len(htimes[htimes == True].index) + len(atimes[atimes == True].index)
+
+def get_gf_by_team(df,item):
+    hg = df.loc[df['HomeTeam'] == item, 'FTHG'].sum()
+    ag = df.loc[df['AwayTeam'] == item, 'FTAG'].sum()
+    return hg + ag
+
+def get_ga_by_team(df,item):
+    ag = df.loc[df['HomeTeam'] == item, 'FTAG'].sum()
+    hg = df.loc[df['AwayTeam'] == item, 'FTHG'].sum()
+    return ag + hg
+
+def generate_output_file(df,filename):
+    df.to_csv(filename, sep = "\t", encoding='utf-8', index = False)
+
+def process_data(df,file_season):
+    global best_scoring_team_by_season, best_lose_to_win_team_df, best_shot_stats_team_df
+    teams = get_teams(df)
+    #- Position Table
+    board = pd.DataFrame(columns=board_cols)
+    score_reverted = pd.DataFrame(columns=score_reverted_cols)
+    shot_stats = pd.DataFrame(columns=shot_stats_cols)
+    #- calculating stats
+    for team in teams:
+        #calc stats
+        team_pts = get_pts_by_team(df,team)
+        team_w = get_w_by_team(df,team)
+        team_l = get_l_by_team(df,team)
+        team_d = get_d_by_team(df,team)
+        team_gf = get_gf_by_team(df,team)
+        team_ga = get_ga_by_team(df,team)
+        team_diff = team_gf - team_ga
+        #Build new DF and add data
+        board.loc[-1] = [team,team_pts,team_w,team_l,team_d,team_gf,team_ga,team_diff,file_season]
+        board.index = board.index + 1
+        board = board.sort_values(by=['Pts'], ascending=False)
+        #-----------Stats by team-------------
+        #scores
+        revert_score = get_best_lose_to_win_team(df,team)
+        score_reverted.loc[-1] = [team,revert_score,file_season]
+        score_reverted.index = score_reverted.index + 1
+        #shots
+        shots_percentage = get_best_shot_stats_by_team(df,team)
+        shot_stats.loc[-1] = [team,shots_percentage,file_season]
+        shot_stats.index = shot_stats.index + 1
+
+    #Collecting data for best scoring team
+    best_scoring_team= board[board['GF']==board['GF'].max()]
+    best_scoring_team_by_season = pd.concat([best_scoring_team_by_season, best_scoring_team[goal_stat_cols]],ignore_index=True, sort=False)
+    #Collecting data for score reverted
+    score_reverted = score_reverted[score_reverted['n_of_matches']==score_reverted['n_of_matches'].max()]
+    best_lose_to_win_team_df = pd.concat([best_lose_to_win_team_df, score_reverted],ignore_index=True, sort=False)
+    #Collecting data for shot stats
+    shot_stats = shot_stats[shot_stats['shot_percentage']==shot_stats['shot_percentage'].max()]
+    best_shot_stats_team_df = pd.concat([best_shot_stats_team_df, shot_stats],ignore_index=True, sort=False)
+
+    return board
+
+
+def main():
+    jsonfiles = []
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-0910_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1011_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1112_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1213_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1314_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1415_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1516_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1617_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1718_json.json')
+    jsonfiles.append('/home/eliseo/Documents/DE_TEST/de-challenge-v2/data/season-1819_json.json')
+
+    position_table_df = pd.DataFrame()
+    for jsonfile in jsonfiles:
+        df = pd.read_json(jsonfile)
+        file_season = get_season_from_file(jsonfile)
+        filtered_data_df = df[cols]
+
+        #- The position table for all the seasons.
+        position_table_df = pd.concat([position_table_df,process_data(filtered_data_df,file_season)], ignore_index=True, sort=False)
+    
+    print('Generating report files...')
+    generate_output_file(position_table_df,'position_table.csv')
+    generate_output_file(best_scoring_team_by_season,'best_scoring_team.csv')
+    generate_output_file(best_shot_stats_team_df,'best_shots_stats.csv')
+    generate_output_file(best_lose_to_win_team_df,'score_reverted.csv')
+
+    print('Finished')
+
+if __name__ == "__main__":
+    print('Starting Process...')
+    main()
